dbb3a7d9498a8c686527ec46cab822a95a77760e
Pedro Souza <pedromartins.souza1999@gmail.com>
2024-12-09 20:57:06 -0300

    add secret generator

---
+++ generate_secret_manifest.sh
@@ -0,0 +1,16 @@
+PAT="<INSERT YOUR PERSONAL ACCESS TOKEN HERE>"
+B64_PAT=$(echo -n $PAT | base64)
+SECRET_CONTENT="{"auths":{"ghcr.io":{"auth":${B64_PAT}}}}"
+B64_SECRET_CONTENT=$(echo -n $SECRET_CONTENT | base64)
+
+SECRET_YAML="kind: Secret \n
+type: kubernetes.io/dockerconfigjson \n
+apiVersion: v1 \n
+metadata: \n
+\t name: dockerconfigjson-github-com \n
+\t labels: \n
+\t\t app: app-name \n
+data: \n
+\t .dockerconfigjson: ${B64_SECRET_CONTENT}"
+
+echo -e $SECRET_YAML > secret.yaml
