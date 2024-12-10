98a10fe411c8e1817694119dd8e56b08584c4967
Pedro Souza <pedromartins.souza1999@gmail.com>
2024-12-09 22:25:12 -0300

    use parameter instead of hardcoded value

--- generate_secret_manifest.sh
+++ generate_secret_manifest.sh
@@ -1,4 +1,4 @@
-PAT="<INSERT YOUR PERSONAL ACCESS TOKEN HERE>"
+PAT=$1
 B64_PAT=$(echo -n $PAT | base64)
 SECRET_CONTENT="{"auths":{"ghcr.io":{"auth":${B64_PAT}}}}"
 B64_SECRET_CONTENT=$(echo -n $SECRET_CONTENT | base64)
